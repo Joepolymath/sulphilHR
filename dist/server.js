8708b8333a18f13be82c43b85a4f07c4b0806fd6
Joepolymath <joshuaajagbe96@gmail.com>
2023-02-22 14:52:18 +0100

    removed dist folder

--- dist/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const app = require('./app');
-const dotenv = require('dotenv');
-dotenv.config();
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Backend is blazing 🔥🔥🔥 @ port ${PORT}`);
-});
